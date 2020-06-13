1dece2449bccdd97917fefeea5fa69b31002338d
EliasMachuanin <eliasmachuanin@gmail.com>
2020-06-13 09:49:13 -0300

    add views and test

---
+++ test/game.test.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+
+describe('Contexto', function() {
+  it('normalizar contexto', function() {
+  });
+});
+
+
+describe('Operar contexto', function() {
+    it('Sumador', function() {
+      expect().equal();
+    });
+  });
