b83d337a4cdb8ea473c74dce4bb2e399408c747d
EliasMachuanin <eliasmachuanin@gmail.com>
2020-06-14 16:39:18 -0300

    probando socket

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import Juego from "./Juego";
+
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
@@ -8,10 +10,17 @@ app.set('views', 'views');
 app.set('view engine', 'html');
 var body_parser = require('body-parser');
 app.use(body_parser.urlencoded({extended:true}));
+var j = new Juego();
 
 app.get('/', function(req, res){
     res.render('login');
   });
+
+  app.post('/',function(req,res){
+    var entrada = req.body.textbox;
+    j.nuevoJugador(entrada)
+    res.render('menu',{locals:{salida: entrada}});
+});
 
   app.get('/tabla', function(req, res){
     res.render('tabla');
@@ -22,20 +31,14 @@ app.get('/', function(req, res){
     res.render('inst');
   });
 
-
-  app.get('/game', function(req, res){
-    res.render('game');
+  app.post('/game', function(req, res){
+    var entrada = req.body.textbox;
+    res.render('game',{locals:{salida: entrada}});
   });
 
 
-  app.post('/',function(req,res){
-    var entrada = req.body.textbox;
-    res.render('menu',{locals:{salida: entrada}});
-});
-
 io.on('connection', function(socket){
   socket.on('nuevoPunto', (data) => {
-      io.emit('nuevoPunto', data)
   })
   socket.on('nuevoUsuarioConectado', (data) => {
     io.emit('nuevoUsuarioConectado', data)
